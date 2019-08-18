0377cdd388906fe705f7ff9928e3c84f77ac9bf8
joeunyeob <naramharam@naver.com>
2019-08-18 21:17:12 +0900

    Django Shell && Admin && CRUD update

--- TravelBlog/blog/admin.py
+++ TravelBlog/blog/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content_size', 'status', 'created_at', 'updated_at']
+
+    actions = ['make_published', 'make_draft']
+
+    def content_size(self, post):
+        return '{}글자'.format(len(post.content))
+    content_size.short_description = '내용 글자수'
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 Published 상태로 변경'.format(updated_count))
+    make_published.short_description = '지정포스팅을 Published 상태로 변경합니다.'
+
+    def make_draft(self, request, queryset):
+        updated_count = queryset.update(status='d')
+        self.message_user(request, '{}건의 포스팅을 Draft 상태로 변경'.format(updated_count))
+    make_draft.short_description = '지정포스팅을 Draft 상태로 변경합니다.'
