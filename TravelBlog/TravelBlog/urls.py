b910320b0a276fbb34c9434a833bf29f689c8457
joeunyeob <naramharam@naver.com>
2019-09-02 22:50:11 +0900

    static file && media file settings

--- TravelBlog/TravelBlog/urls.py
+++ TravelBlog/TravelBlog/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.views.generic import RedirectView
@@ -19,6 +20,9 @@ urlpatterns = [
     url(r'^shop/', include('shop.urls')),
     url(r'^dojo/', include('dojo.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)  # media 파일 추가시 작성 해줘야 된다.
+
 
 if settings.DEBUG:
     import debug_toolbar
