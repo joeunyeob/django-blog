d892837ffd0f597a18b5f38c390be865f1f117be
joeunyeob <naramharam@naver.com>
2019-09-07 21:39:46 +0900

    paginate , CBV(Deatail, List, Update, Create, Delete) upload

--- TravelBlog/blog/views_cbv.py
+++ TravelBlog/blog/views_cbv.py
@@ -1,17 +1,15 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .models import Post
 from django import forms
 
+post_list = ListView.as_view(model=Post, paginate_by=3)
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = '__all__'
+post_detail = DetailView.as_view(model=Post)
+
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
+
+post_new = CreateView.as_view(model=Post)
+
+post_delete = DeleteView.as_view(model=Post, success_url='/blog/')
 
 
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
-
-
-post_new = PostCreateView.as_view()
